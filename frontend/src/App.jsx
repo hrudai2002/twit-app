b5f8abe69a075f1221ef7e0ddb712d5e3156dfac
hrudai2002 <hrudai2002@gmail.com>
2023-08-21 23:13:07 +0530

    completed the first basic login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import LoginPage from './screens/login';
+import { LeftBar } from './components/LeftBar/left-bar';
 
 
 function App() {
   return (
     <LoginPage />
+    // <LeftBar />
   )
 }
 
