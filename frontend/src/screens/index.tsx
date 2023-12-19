c8b46eadd1373f47fa6cd4fbe20efaf387772759
hrudai2002 <hrudai2002@gmail.com>
2023-12-19 12:37:33 +0530

    code formatting in index.tsx

--- frontend/src/screens/index.tsx
+++ frontend/src/screens/index.tsx
@@ -17,17 +17,15 @@ import { MdOutlineGifBox } from "react-icons/md";
 import { BsEmojiSmile } from "react-icons/bs";
 import { RiListRadio } from "react-icons/ri";
 import { GrSchedulePlay } from "react-icons/gr";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa"
+
+const SelectedTabs = {
+    For_You: 'For you', 
+    Following: 'Following'
+}
 
 
-
-
-
-function App() {
-
-    const user = useContext(UserContext);
-    const [selectedTab, setSelectedTab] = useState('For you');
-
+function Navbar(props: any) {
     const navbar = [
         {
             icon: <RiHome5Line fontSize={24} />, 
@@ -66,6 +64,109 @@ function App() {
             name: 'More'
         }
     ]
+    return (
+        <div className="navbar">
+            <div className="navbar-container">
+                <FaTwitter fontSize={30} />
+                {
+                    navbar.map((row, index) => (
+                        <div key={index} className="list-item">
+                            <div>{row.icon}</div>
+                            <div>{row.name}</div>
+                        </div>
+                    ))
+                }
+                <a href="" className='post-btn'>Post</a>
+                <div className="profile">
+                    <img className='user-dp' src={props.user?.imageUrl} alt="" />
+                    <div className='user'>
+                        <div className='fullname'>{props.user?.name}</div>
+                        <div className='username'>@{props.user?.name}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+function Feed(props: any) {
+    return (
+        <div className="feed">
+            <div className="feed-header">
+                <div className='feed-header-nav'>
+                    <div
+                        className="for-you"
+                        style={{
+                            color: props.selectedTab === SelectedTabs.For_You ? '#fff' : '#5f6467',
+                            fontWeight: props.selectedTab === SelectedTabs.For_You ? 'bold' : 'normal'
+                        }}
+                        onClick={() => props.changeTab(SelectedTabs.For_You)}
+                    >
+                        <div
+                            className='text'
+                            style={{
+                                borderBottom: props.selectedTab === SelectedTabs.For_You ? '2px solid #1c9bef' : 'none'
+                            }}>For you</div>
+                    </div>
+                    <div
+                        className="following"
+                        style={{
+                            color: props.selectedTab === SelectedTabs.Following ? '#fff' : '#5f6467',
+                            fontWeight: props.selectedTab === SelectedTabs.Following ? 'bold' : 'normal'
+                        }}
+                        onClick={() => props.changeTab(SelectedTabs.Following)}
+                    >
+                        <div className='text'
+                            style={{
+                                borderBottom: props.selectedTab === SelectedTabs.Following ? '2px solid #1c9bef' : 'none'
+                            }}>Following</div>
+                    </div>
+                </div>
+                <div className="settings-icon">
+                    <FiSettings />
+                </div>
+            </div>
+
+            <div className="create-post">
+                <div className="top-part">
+                    <img className='user-dp' src={props.user?.imageUrl} alt="" />
+                    <textarea rows={2} id="post-input" placeholder='What is happening?!'></textarea>
+                </div>
+                <div className="bottom-part">
+                    <div></div>
+                    <div className="media-icons">
+                        <FaRegImage fontSize={20} />
+                        <MdOutlineGifBox fontSize={20} />
+                        <RiListRadio fontSize={20} />
+                        <BsEmojiSmile fontSize={20} />
+                        <GrSchedulePlay fontSize={20} />
+                        <FaMapMarkerAlt fontSize={20} opacity={0.5} />
+                    </div>
+                    <a href="" className='post-button'>Post</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function SideBar(props: any) {
+    return (
+        <div className="sidebar">
+            <div className="search-input">
+                <CiSearch />
+                <input type="text" name="" id="Search" />
+            </div>
+        </div>
+    )
+}
+
+
+function App() {
+
+    const user = useContext(UserContext);
+    const [selectedTab, setSelectedTab] = useState(SelectedTabs.For_You);
 
     const changeTab = (tab: string) => {
         setSelectedTab(tab);
@@ -73,83 +174,9 @@ function App() {
 
     return (
         <div className="home">
-            <div className="navbar">
-                <div className="navbar-container">
-                    <FaTwitter fontSize={30} />
-                    {
-                        navbar.map((row, index) => (
-                            <div key={index} className="list-item">
-                                <div>{ row.icon }</div>
-                                <div>{ row.name }</div>
-                            </div>
-                        ))
-                    }
-                    <a href="" className='post-btn'>Post</a>
-                    <div className="profile">
-                        <img className='user-dp' src={user?.imageUrl} alt="" />
-                        <div className='user'>
-                            <div className='fullname'>{ user?.name }</div>
-                            <div className='username'>@{user?.name}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="feed">
-                <div className="feed-header">
-                    <div className='feed-header-nav'>
-                        <div 
-                        className="for-you" 
-                        style={{ 
-                            color: selectedTab === 'For you' ?  '#fff' : '#5f6467',
-                            fontWeight: selectedTab === 'For you' ? 'bold' : 'normal'
-                        }}
-                        onClick={() => changeTab('For you')}
-                        >
-                            <div 
-                            className='text'
-                            style={{
-                                borderBottom: selectedTab === 'For you' ? '2px solid #1c9bef' : 'none'
-                            }}>For you</div>
-                        </div>
-                        <div 
-                        className="following" 
-                        style={{
-                            color: selectedTab === 'Following' ? '#fff' : '#5f6467', 
-                            fontWeight: selectedTab === 'Following' ? 'bold' : 'normal'
-                        }}
-                        onClick={() => changeTab('Following')}
-                        >
-                            <div className='text'
-                            style={{
-                                borderBottom: selectedTab === 'Following' ? '2px solid #1c9bef' : 'none'
-                            }}>Following</div>
-                        </div>
-                    </div>
-                    <div className="settings-icon">
-                        <FiSettings />
-                    </div>
-                </div>
-
-                <div className="create-post">
-                    <div className="top-part">
-                       <img className='user-dp' src={user?.imageUrl} alt="" />
-                        <textarea rows={2} id="post-input" placeholder='What is happening?!'></textarea>
-                    </div>
-                    <div className="bottom-part">
-                        <div></div>
-                        <div className="media-icons">
-                            <FaRegImage fontSize={20} />
-                            <MdOutlineGifBox fontSize={20} />
-                            <RiListRadio fontSize={20} />
-                            <BsEmojiSmile fontSize={20} />
-                            <GrSchedulePlay fontSize={20} />
-                            <FaMapMarkerAlt fontSize={20} opacity={0.5} />
-                        </div>
-                        <a href="" className='post-button'>Post</a>
-                    </div>
-                </div>
-            </div>
-            <div className="sidebar"></div>
+            <Navbar user={user} />
+            <Feed user={user} selectedTab={selectedTab} changeTab={changeTab} />
+            <SideBar />
         </div>
     )
 }
